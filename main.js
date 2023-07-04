1799cf258fe17b8983ddb56bb5027b5f4c385460
fatgeeah <fatgeeahminkis@gmail.com>
2023-07-04 14:27:47 +0200

    some functionality

---
+++ main.js
@@ -0,0 +1,17 @@
+//display on click
+const modalWrapper = document.querySelector(".modals-wrapper");
+if (modalWrapper) {
+    function displayModal(id) {
+        const modal = document.getElementById(id);
+        modalWrapper.style.display = "flex";
+        modal.style.display = "flex";
+
+        //close
+        const close = document.getElementById("close-modal");
+        close.addEventListener("click", () => {
+            modalWrapper.style.display = "none";
+            modal.style.display = "none";   
+        } )
+    }
+}
+
